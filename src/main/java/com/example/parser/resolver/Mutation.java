1d1317c1b30dfe6e428ba85747eedcfd372f8a2c
shubhamd <shubhamd@in-shubhamd.local>
2020-04-12 19:27:22 +0530

    added save All Countries

--- src/main/java/com/example/parser/resolver/Mutation.java
+++ src/main/java/com/example/parser/resolver/Mutation.java
@@ -5,6 +5,8 @@ import com.example.parser.entity.Country;
 import com.example.parser.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class Mutation implements GraphQLMutationResolver {
@@ -17,5 +19,16 @@ public class Mutation implements GraphQLMutationResolver {
         countryRepository.save(country);
 
         return country;
+    }
+
+    public String[] saveAllCountries(String[] names) {
+        List<String> countryCollection = Arrays.asList(names);
+        countryCollection.stream()
+                .map(s -> {
+                    Country country = Country.builder().name(s).build();
+                    return countryRepository.save(country);
+                });
+
+        return names;
     }
 }
